863eca6655639be1a689596354853757974b25ea
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2024-10-28 17:22:31 +0800

    feat: dir

--- functions/mod.ts
+++ functions/mod.ts
@@ -2,6 +2,3 @@ export * from './chain.ts';
 export * from './modules/module-registry.ts';
 export * from './postgres.ts';
 export * from './supabase-client.ts';
-
-export * from './factory/send-tx.ts';
-export * from './factory/sign-block.ts';
