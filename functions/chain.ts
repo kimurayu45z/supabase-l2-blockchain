e45c2d182d0da92bbaa17504365bc9f579365d65
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2024-10-28 17:26:52 +0800

    feat: chain id

--- functions/chain.ts
+++ functions/chain.ts
@@ -1,15 +1,9 @@
 import type { ModuleRegistry } from './modules/module-registry.ts';
 
 export class Chain<Schema extends Record<string, unknown>> {
-	id: string;
 	constructor(
+		public id: string,
 		public db: { url: string; schema: Schema },
 		public moduleRegistry: ModuleRegistry<Schema>
-	) {
-		const chainId = Deno.env.get('CHAIN_ID');
-		if (!chainId) {
-			throw Error('Environment variable CHAIN_ID is not configured');
-		}
-		this.id = chainId;
-	}
+	) {}
 }
