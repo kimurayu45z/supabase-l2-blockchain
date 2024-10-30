3fa242be918ae4382e217e8c25b80d497bb02d9b
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2024-10-30 14:01:19 +0800

    finalize publish

--- functions/core/chain.ts
+++ functions/core/chain.ts
@@ -1,16 +1,11 @@
 import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js/driver';
 
-import { postgresDatabase } from '../core/postgres.ts';
 import type { ModuleRegistry } from '../modules/module-registry.ts';
 
 export class Chain<Schema extends Record<string, unknown>> {
-	db: PostgresJsDatabase<Schema>;
-
 	constructor(
 		public id: string,
-		db: { url: string; schema: Schema },
+		public db: PostgresJsDatabase<Schema>,
 		public moduleRegistry: ModuleRegistry<Schema>
-	) {
-		this.db = postgresDatabase(db.url, db.schema);
-	}
+	) {}
 }
--- functions/mod.ts
+++ functions/mod.ts
@@ -1,4 +1,2 @@
 export * from './core/chain.ts';
 export * from './modules/module-registry.ts';
-export * from './core/postgres.ts';
-export * from './core/supabase-client.ts';
